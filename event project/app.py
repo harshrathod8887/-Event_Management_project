5c5bb6d8cd01bc7f743b2e48cdb0ea66fefcb601
Harsh_rathod_8887 <163307818+harshrathod8887@users.noreply.github.com>
2026-06-27 13:12:51 +0530

    Update app.py

--- event project/app.py
+++ event project/app.py
@@ -326,6 +326,6 @@ def certificate(id):
 
 
 if __name__ == "__main__":
-    create_table()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
  
